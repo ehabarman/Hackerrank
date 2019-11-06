4b0e921c20c2d38d4f552c4bc649edf1d2ebddcb
ehaba <ehabarman@gmail.com>
2019-11-06 21:09:04 +0200

    6-11-2019

---
+++ src/Regex/Applications/easy/DetectHTMLTags.java
@@ -0,0 +1,23 @@
+package Regex.Applications.easy;
+
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class DetectHTMLTags {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        Pattern pattern = Pattern.compile("<([a-z][a-z0-9]*)[^>]*>");
+        HashSet<String> set = new HashSet<String>();
+        while(n-->=0){
+            String line = s.nextLine();
+            Matcher m = pattern.matcher(line);
+            while(m.find())
+                set.add(m.group(1));
+        }
+        ArrayList<String> list = new ArrayList<>(set);
+        Collections.sort(list);
+        System.out.println(String.join(";", list));
+    }
+}
---
+++ src/Regex/Assertions/easy/NegativeLookahead.java
@@ -0,0 +1,32 @@
+package Regex.Assertions.easy;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class NegativeLookahead {
+
+    public static void main(String[] args) {
+
+        NegativeLookahead.Regex_Test tester = new NegativeLookahead.Regex_Test();
+        tester.checker("(.)(?!\\1)");
+
+    }
+
+    static class Regex_Test {
+
+        public void checker(String Regex_Pattern) {
+
+            Scanner Input = new Scanner(System.in);
+            String Test_String = Input.nextLine();
+            Pattern p = Pattern.compile(Regex_Pattern);
+            Matcher m = p.matcher(Test_String);
+            int Count = 0;
+            while (m.find()) {
+                Count += 1;
+            }
+            System.out.format("Number of matches : %d", Count);
+        }
+
+    }
+}
